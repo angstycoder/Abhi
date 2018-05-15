0d2d070d779206cd951c409cbff510a8de5db246
Abhi <39215553+angstycoder@users.noreply.github.com>
2018-05-15 20:26:20 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
 import discord
 import asyncio
 import json
-
+import  random
 client = discord.Client()
 
+def ran():
+    r=int(random.random()*10)
+    return (r)
 with open('token.json') as secret:
     token = json.load(secret)
 @client.event
@@ -21,10 +24,25 @@ async def on_message(message):
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
-
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+
     elif message.content.startswith('?a sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
+    resp=['What!','What do you mean by that??',
+          'You need a random number?',
+          'Pfft no!','Me no do it!!',
+          'Its not like i get paid for this -_-']
+    if message.content.startswith('?a random'):
+        tp = await client.send_message(message.channel, 'Random Number? ')
+        while resp:
+            a=random.choice(resp)
+            tp = await client.edit_message(tp,a)
+            resp.remove(a)
+            await asyncio.sleep(2.5)
+
+        await asyncio.sleep(1)
+        await client.edit_message(tp,'It is {}.'.format(ran()))
+
 
 client.run(token)
